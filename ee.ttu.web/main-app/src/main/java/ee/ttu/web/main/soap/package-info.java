1ccc7f48ba80b7d91e68ff86f29eec1c2425f780
Reimo.Vellemaa <reimo.vellemaa@ttu.ee>
2017-05-16 12:30:13 +0300

    rewrite

--- ee.ttu.web/main-app/src/main/java/ee/ttu/web/main/soap/package-info.java
+++
@@ -1,3 +0,0 @@
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://ttu.ee/web/delivery-info", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package ee.ttu.web.main.soap;
--- ee.ttu.web/remote-ttu-mock/src/main/java/ee/ttu/web/repository/CouriersRepository.java
+++
@@ -1,9 +0,0 @@
-package ee.ttu.web.repository;
-
-import ee.ttu.web.domain.Courier;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CouriersRepository extends JpaRepository<Courier, Long> {
-}
